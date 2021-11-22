7631d9c2a3078e29f66464e85ef198a94bf4c6f2
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2021-11-22 06:53:54 +0100

    wip

---
+++ Sources/TVShowsRxService/Service/ShowService.swift
@@ -0,0 +1,23 @@
+//
+//  ShowService.swift
+//  
+//
+//  Created by iMacPro on 22.11.2021..
+//
+
+import Foundation
+
+class ShowService {
+  
+  static let shared = ShowService()
+  
+  // TODO: allShows -
+  // TODO: oneShow(showId) -
+  // TODO: topRatedShows -
+  
+  // TODO: allReviews(showId) -
+  // TODO: createReview(showId) -
+  // TODO: deleteReview(reviewId) -
+  // TODO: updateReview(reviewId) -
+  
+}
