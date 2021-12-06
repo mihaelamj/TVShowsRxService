54a986b505dcb307d57e6666bb060e3f5a6f5d3e
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2021-12-06 21:49:41 +0100

    Added a new package `TVShowsModels`

--- Package.swift
+++ Package.swift
@@ -15,11 +15,12 @@ let package = Package(
     .package(url: "https://github.com/mihaelamj/RequestAdapter.git", from: "0.2.0"),
     .package(url: "https://github.com/mihaelamj/TVShowsEndpoint.git", branch: "main"),
     .package(url: "https://github.com/mihaelamj/TVShowsResponseResults.git", branch: "main"),
+    .package(url: "https://github.com/mihaelamj/TVShowsModels.git", branch: "main"),
     .package(url: "https://github.com/ReactiveX/RxSwift.git", branch: "main")
   ],
   targets: [
     .target(
-      name: "TVShowsRxService", dependencies: ["Alamofire", "RequestAdapter", "TVShowsEndpoint", "TVShowsResponseResults", "RxSwift"]),
+      name: "TVShowsRxService", dependencies: ["Alamofire", "RequestAdapter", "TVShowsEndpoint", "TVShowsResponseResults", "TVShowsModels", "RxSwift"]),
     .testTarget(
       name: "TVShowsRxServiceTests",
       dependencies: [
